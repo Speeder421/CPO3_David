ad93bdfad1562c21bfd5c2df7bed767f9f6f253c
Speeder421 <davidjesselim4@gmail.com>
2025-07-20 03:58:10 +0000

    Jul 20, 2025, 3:58 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -40,7 +40,7 @@ function draw() {
   if(ball.y<0+ball.diameter/2||ball.y>height-ball.diameter/2){
     ball.vel.y*=-1.05
     }
-  if(box.x<0+box.diameter/2||box.x>width-box.diameter/2){
+  if(box.x<0+box.w/2||box.x>width-box.diameter/2){
     box.vel.x*=-1.05
     }
 }
