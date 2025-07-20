8979b536fbc63c0319b825dda2094a39fc2719ad
Speeder421 <davidjesselim4@gmail.com>
2025-07-20 03:31:32 +0000

    Jul 20, 2025, 3:31 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -7,7 +7,7 @@ function setup() {
   // write your codes here
   // End Basic shape testing
 Fill("skyblue")
-
+stroke("pink")
   // Create a bouncing ball sprite
   // write your codes here
 ball=new Sprite();
