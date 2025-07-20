84260e119d2c2f420575a171f04f6e0359b7028a
Speeder421 <davidjesselim4@gmail.com>
2025-07-20 03:33:22 +0000

    Jul 20, 2025, 3:33 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -6,17 +6,17 @@ function setup() {
   // Basic shape testing
   // write your codes here
   // End Basic shape testing
-Fill("skyblue")
-stroke("pink")
-strokeWeight(10)
-circle(30,30,50)
-rect(50,50,100,200)
+  Fill("skyblue")
+  stroke("pink")
+  strokeWeight(10)
+  circle(30,30,50)
+  rect(50,50,100,200)
   // Create a bouncing ball sprite
   // write your codes here
-ball=new Sprite();
-ball.x=100;
-ball.y=200;
-ball.diameter=40;
+  ball=new Sprite();
+  ball.x=100;
+  ball.y=200;
+  ball.diameter=40;
 ball.color="red"
 ball.vel.x=3;
 ball.vel.y=3;
