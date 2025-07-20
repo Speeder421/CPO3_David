c7186c64bc255f8c364cddd3463fa1b2c6898c4d
Speeder421 <davidjesselim4@gmail.com>
2025-07-20 04:08:42 +0000

    Jul 20, 2025, 4:08 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -39,13 +39,13 @@ function draw() {
   textSize(16);
   text("Ball:("+int(ball.x)+","+int(ball.y)+")",10,20);
   text("Mouse:("+int(mousex)+","+int(mousey)+")",10,40);
-  
+
   if(ball.x<0+ball.diameter/2||ball.x>width-ball.diameter/2){
     ball.vel.x*=-1
   }
   if(ball.y<0+ball.diameter/2||ball.y>height-ball.diameter/2){
     ball.vel.y*=-1
-    }
+  }
   box.x=mouse.x
   box.y=mouse.y
 }
