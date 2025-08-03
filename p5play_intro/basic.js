e92757a5aba5c9842f5e6517f772c6c97ed02dff
Speeder421 <davidjesselim4@gmail.com>
2025-08-03 03:14:52 +0000

    Aug 3, 2025, 3:14 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -34,7 +34,7 @@ groceries.push('orange');
 groceries.push('butter');
 groceries.shift();
 groceries.splice(1,0,"kaya");
-console.log(groceries[])
+console.log(groceries[4]);
 for(let item of groceries){
   console.log(item);
 }
