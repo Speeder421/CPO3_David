e0a4f8a9d5172fbef6175af3e276fa479a035871
Speeder421 <davidjesselim4@gmail.com>
2025-08-10 03:44:44 +0000

    Aug 10, 2025, 3:44 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -4,6 +4,7 @@ function preload(){
   flapMidImg=loadImage('assets/yellowbird-midflap.png');
   bg=loadImage('assets/background-day.png');
   base=loadImage('assets/base.png');
+  flapup
 }
 
 function setup(){
