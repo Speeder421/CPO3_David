c0e9c2a54776741334eaee21e82361626dbf7130
Speeder421 <davidjesselim4@gmail.com>
2025-08-10 03:03:54 +0000

    Aug 10, 2025, 3:03 AM

---
+++ flappybird/playground/playground.js
@@ -0,0 +1,35 @@
+let bird,floor;
+let flapMidImg,bg,base;
+function preload(){
+  flapMidImg=loadImage('assets/yellowbird-midflap.png');
+  bg=loadImage('assets/background-day.png');
+  base=loadImage('assets/base.png');
+}
+
+function setup(){
+  new Canvas(400,600);
+  bird= new Sprite();
+  bird.x=width/2;
+  bird.y=200;
+  bird.width=30;
+  bird.height=30;
+  bird.img=flapMidImg;
+  bird.collider="dynamic";
+  bird.mass=2;
+  bird.drag=0.02;
+  bird.bounciness=0.5;
+  world.gravity.y=10;
+
+  floor=new Sprite();
+  floor.x=200;
+  floor.y=height-20;
+  floor.width=400;
+  floor.height=125;
+  floor.collider="static"
+  floor.img=base;
+}
+
+function draw(){
+  image(bg,0,0,width,height);
+  
+}
