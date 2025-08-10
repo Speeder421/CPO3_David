f2c570c475c244cf93d3e4f63bdbf9d72f8bdc8d
Speeder421 <davidjesselim4@gmail.com>
2025-08-10 03:57:37 +0000

    Aug 10, 2025, 3:57 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -40,7 +40,7 @@ function draw(){
   text("isMoving:"+bird.isMoving,10,40);
   text("sleeping:"+bird.sleeping,10,60);
 
-  if(kb.presses('space')||mouse.presses){
+  if(kb.presses('space')||mouse.presses()){
     bird.vel.y=-5;
     bird.sleeping=false;
   }
