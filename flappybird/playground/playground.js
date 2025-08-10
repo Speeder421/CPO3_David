3d165d4d21cbd436adadf3e1f159e1ff861f2180
Speeder421 <davidjesselim4@gmail.com>
2025-08-10 04:16:19 +0000

    Aug 10, 2025, 4:16 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -49,7 +49,7 @@ function draw(){
 //   }
   if(bird.vel.y<-1){
     bird.img=flapUpImg;
-    for(leti=0;i<45;i++){
+    for(leti=0;i<-45;i++){
         bird.rotation=-1;
     }
     
@@ -58,9 +58,9 @@ function draw(){
   else if(bird.vel.y>1){
     bird.img=flapDownImg;
     for(leti=0;i<45;i++){
-        bird.rotation=-1;
+        bird.rotation=45;
     }
-    bird.rotation=45;
+    
   }
   else{
     bird.img=flapMidImg;
