53550c25f8a9972bbac982533045cf822099ce69
Speeder421 <davidjesselim4@gmail.com>
2025-08-17 03:40:09 +0000

    Aug 17, 2025, 3:40 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -75,7 +75,7 @@ function draw(){
 
 }
 function spawnPipePair(){
-  let gap=;
+  let gap=20;
   let midY=height/2;
   bottomPipe=new Sprite(100,midY+gap+200,52,320,"static");
   bottomPipe.img=pipeImg;
