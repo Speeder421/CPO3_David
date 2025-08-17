ace73c6716f486a276a5bcb49a0be69b51dc0cdf
Speeder421 <davidjesselim4@gmail.com>
2025-08-17 03:29:54 +0000

    Aug 17, 2025, 3:29 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -78,7 +78,6 @@ function spawnPipePair(){
   bottomPipe=new Sprite(100,height/2,52,320,"static");
   bottomPipe.img=pipeImg;
   pipeGroup.add(bottomPipe);
-  pipeGroup.layer
+  pipeGroup.layer=0
 
 }
-pipeGroup
