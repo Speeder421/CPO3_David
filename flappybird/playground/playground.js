eb469b743718fb5a3e502ded6cff9ebd15307b79
Speeder421 <davidjesselim4@gmail.com>
2025-08-24 03:57:43 +0000

    Aug 24, 2025, 3:57 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -27,8 +27,8 @@ function setup() {
   bird = new Sprite();
   bird.x = width / 2;
   bird.y = 200,
-  bird.width = 30;
-  bird.height = 30;
+  bird.width = 34;
+  bird.height = 24;
   bird.img = flapMidImg; 
   bird.visible = false;
 
