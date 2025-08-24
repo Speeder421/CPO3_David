2bbdfc2df44d104e977678d908179295cbef7298
Speeder421 <davidjesselim4@gmail.com>
2025-08-24 03:30:54 +0000

    Aug 24, 2025, 3:30 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -44,7 +44,7 @@ function setup(){
   pipeGroup=new Group();
 
   startScreenLabel = new Sprite(width/2,height/2,50,50,none);
-  startScreenLabel.img=startScreenImg
+  startScreenLabel.img=startScreenImg;
 
 
 
