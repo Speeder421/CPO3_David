548663a6fa453fae9164a0ef7e4d0be81858dfcc
Speeder421 <davidjesselim4@gmail.com>
2025-08-24 03:17:52 +0000

    Aug 24, 2025, 3:17 AM

--- flappybird/playground/pg4_camera.js
+++ flappybird/playground/pg4_camera.js
@@ -4,7 +4,7 @@ let flapDownImg,flapUpImg;
 let pipeImg;
 let pipeGroup;
 let bottomPipe,topPipe;
-let gameover
+let gameOver;
 function preload(){
   flapMidImg=loadImage('assets/yellowbird-midflap.png');
   bg=loadImage('assets/background-day.png');
@@ -12,6 +12,7 @@ function preload(){
   flapUpImg=loadImage('assets/yellowbird-upflap.png')
   flapDownImg=loadImage('assets/yellowbird-downflap.png')
   pipeImg=loadImage('assets/pipe-green.png');
+  
 }
 
 function setup(){
