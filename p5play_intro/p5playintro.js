231fb95dc50a92a1cba9e83d3502f2ada8e5a702
Speeder421 <davidjesselim4@gmail.com>
2025-08-31 04:15:37 +0000

    Aug 31, 2025, 4:15 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -35,7 +35,7 @@ function setup() {
 function draw() {
   // write your codes here
   background(250);
-  
+  if(KeyboardEvent.pre)
   // fill(0);
   // textSize(16);
   // text("Ball:("+int(ball.x)+","+int(ball.y)+")",10,20);
