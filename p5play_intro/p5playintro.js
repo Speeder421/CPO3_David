c611a57fe37ea6633341a938f374c14d0b31d6d4
Speeder421 <davidjesselim4@gmail.com>
2025-08-31 04:22:29 +0000

    Aug 31, 2025, 4:22 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -55,7 +55,7 @@ function draw() {
   // box.y=mouse.y
 }
 
-function getRandomIntInclusive(min, max) {
+function getRandom(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
