04afdf3aeac3da6ee260a60b2b911e59cc27faa7
Speeder421 <davidjesselim4@gmail.com>
2025-08-31 04:18:46 +0000

    Aug 31, 2025, 4:18 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -2,7 +2,7 @@ function setup() {
   // Set up the canvas
   new Canvas(800, 400);
   background(250); //background color
-
+  let random=Math.random
   // Basic shape testing
   // write your codes here
   // End Basic shape testing
