91f75b4e237b27da1208fdca785a0369ee2e78a2
Speeder421 <davidjesselim4@gmail.com>
2025-08-31 04:28:54 +0000

    Aug 31, 2025, 4:28 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -15,23 +15,7 @@ function setup() {
   // rect(50,50,100,200);
   // Create a bouncing ball sprite
   // write your codes here
-  ball=new Sprite();
-  ball.x=100;
-  ball.y=200;
-  ball.diameter=40;
-  ball.color="red"
-  ball.vel.x=3;
-  ball.vel.y=3;
-  ball.bounciness=1;
-  ball.collider="dynamic";
 
-  box= new Sprite();
-  box.x=100;
-  box.y=100;
-  box.w=50;
-  box.h=50;
-  box.color="green";
-  box.collider="solid";
 }
 
 function draw() {
