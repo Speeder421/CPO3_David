0e77a84baba74535be374960a87a55c3507fd2c1
Speeder421 <davidjesselim4@gmail.com>
2025-09-07 03:24:11 +0000

    Sep 7, 2025, 3:24 AM

--- p5play_intro/testanswer.js
+++ p5play_intro/testanswer.js
@@ -5,10 +5,13 @@ function setup(){
     new Canvas(600,400);
     ball = new Sprite();
     ball.y=0;
-    ball.x=
+    ball.x=300;
     ball.diameter=40;
     world.gravity.y=1;
 }
 function draw(){
     background(225);
+    if(mouse.pressed){
+        new Sprite(mos)
+    }
 }
