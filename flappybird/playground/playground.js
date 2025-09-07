e9d726ec3d6e64605e554847c679ed773d9050c9
Speeder421 <davidjesselim4@gmail.com>
2025-09-07 04:05:22 +0000

    Sep 7, 2025, 4:05 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -115,7 +115,7 @@ function draw() {
 
 
     if (bird.collides(pipeGroup) || bird.collides(floor)){
-      gameoverLabel = new Sprite(width/2, height/2, 192, 42);
+      gameoverLabel = new Sprite(width/2, height/2, 192, 42,"none");
       gameoverLabel.img = gameoverImg;
       gameoverLabel.layer = 100;
       gameoverLabel.x = camera.x;
