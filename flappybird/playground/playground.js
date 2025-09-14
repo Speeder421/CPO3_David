a1235a7fe59c94313b3b66670aef22f9bdbfe845
Speeder421 <davidjesselim4@gmail.com>
2025-09-14 03:34:58 +0000

    Sep 14, 2025, 3:34 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -155,6 +155,6 @@ function drawScore(x,y,score,digitWidth,digitHeight){
   let startX=x-totalWidth/2;
   for(let i=0;i<scoreStr.length;i++){
     let digit=int(scoreStr[1]);
-    
+    let xpos=startx
   }
 }
