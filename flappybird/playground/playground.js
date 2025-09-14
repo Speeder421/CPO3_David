77607516ced2d09e87c521906c481e48952cabc2
Speeder421 <davidjesselim4@gmail.com>
2025-09-14 03:48:07 +0000

    Sep 14, 2025, 3:48 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -158,7 +158,7 @@ function drawScore(x,y,score,digitWidth,digitHeight){
     let xpos=startX+i*digitWidth;
     let digitSprite=new scoreDigits.Sprite(xpos,y,digitWidth,digitHeight);
     digitSprite.img=list[digit];
-    movegroup()
+    movegroup(scoreDigits,camerax)
   }
 }
 function movegroup(group,targetx,spacing){
