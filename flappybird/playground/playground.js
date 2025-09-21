025d9d5efc9e11df8ca31852546824d8e448f1f4
Speeder421 <davidjesselim4@gmail.com>
2025-09-21 03:14:36 +0000

    Sep 21, 2025, 3:14 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -141,6 +141,7 @@ function spawnPipePair(){
   topPipe = new Sprite(bird.x + 400, midY - gap / 2 - 200, 52, 320, 'static');
   topPipe.img = pipe;
   topPipe.rotation = 180;
+  top
   bottomPipe = new Sprite(bird.x + 400, midY + gap / 2 + 200, 52, 320, 'static');
   bottomPipe.img = pipe;
   pipeGroup.add(topPipe);
