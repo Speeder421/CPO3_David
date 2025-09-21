4de301792bf99247b8cbdf536db5575cc7459812
Speeder421 <davidjesselim4@gmail.com>
2025-09-21 03:25:27 +0000

    Sep 21, 2025, 3:25 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -73,7 +73,8 @@ function draw() {
     let piperight=pipe.x+(pipe.w/2);
     let birdleft=bird.x-(bird.w/2);
     if(pipe.passed=false||piperight<birdleft){
-      pipe.passed=true
+      pipe.passed=true;
+      score
     }
   }
 
