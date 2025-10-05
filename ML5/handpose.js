1b7a2eeeafd451ea33ade5e8e9bcd333c9e6f918
Speeder421 <davidjesselim4@gmail.com>
2025-10-05 03:43:58 +0000

    Oct 5, 2025, 3:43 AM

--- ML5/handpose.js
+++ ML5/handpose.js
@@ -35,7 +35,7 @@ function draw(){
 
     for(let i=0;i<hands.length;i++){
         let hand=hands[i];
-        for(let j=0;j<hands.keypoints.length;j++){
+        for(let j=0;j<hand.keypoints.length;j++){
             let keypoint=hand.keypoints[j];
             circle(keypoint.x,keypoint.y,10);
         }
