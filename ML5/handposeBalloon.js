a2f617fff789b2c0cf860846f4c0c13d34064194
Speeder421 <davidjesselim4@gmail.com>
2025-10-05 04:22:29 +0000

    Oct 5, 2025, 4:22 AM

--- ML5/handposeBalloon.js
+++ ML5/handposeBalloon.js
@@ -17,7 +17,7 @@ function setup(){
     fingerTip=new Sprite();
     fingerTip.diameter=60;
     fingerTip.collider="kinmetic";
-    fingerTip.color=rgba(52,255,)
+    fingerTip.color='rgba(52,255,2,0.05)';
 
 
     let constraints={
