23a0a67be7bde18560aa9bfe772893ff6e2d2846
Speeder421 <davidjesselim4@gmail.com>
2025-10-05 04:07:39 +0000

    Oct 5, 2025, 4:07 AM

---
+++ ML5/handposeBalloon.js
@@ -0,0 +1,46 @@
+let handPose;
+let videoW=640,videoH=480;
+let video;
+let hands=[];
+function preload(){
+    let options={
+        flipped:true,
+        runtime:'tfjs',
+        modeltype:'lite',
+        detectorModelURL:undefined,
+        landMarkModel:undefined,
+    }
+    handPose=ml5.handPose(options);
+}
+function setup(){
+    createCanvas(videoW,videoH)
+    let constraints={
+        video:{
+            mandatory:{
+                minWidth:videoW,
+                minHeight:videoH,
+            },
+            optional:[{minFrameRate:60}]
+        },
+        audio:false,
+        flipped:true,
+    };
+    video=createCapture(constraints);
+    video.size=(640,480);
+    video.hide();
+    handPose.detectStart(video,gotHands);
+}
+function draw(){
+    image(video,0,0,videoW,videoH);
+
+    for(let i=0;i<hands.length;i++){
+        let hand=hands[i];
+        for(let j=0;j<hand.keypoints.length;j++){
+            let keypoint=hand.keypoints[j];
+            circle(keypoint.x,keypoint.y,10);
+        }
+    }
+}
+function gotHands(result){
+    hands=result;
+}
