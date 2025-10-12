3fccf89c83f2fec628e3e5651d7561c0659d739e
Speeder421 <davidjesselim4@gmail.com>
2025-10-12 03:52:24 +0000

    Oct 12, 2025, 3:52 AM

--- p5play_intro/basic.input.js
+++ p5play_intro/basic.input.js
@@ -3,7 +3,8 @@ let usertext="";
 function setup(){
     createCanvas(600,400);
     inputtext=createInput();
-    inputtext.position(200,)
+    inputtext.position(200,height-80);
+    
 
 }
 function draw(){
