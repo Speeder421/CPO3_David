c7016510c37749e865dc723a1c931423c40284dc
Speeder421 <davidjesselim4@gmail.com>
2025-10-12 04:15:46 +0000

    Oct 12, 2025, 4:15 AM

--- p5play_intro/basic.input.js
+++ p5play_intro/basic.input.js
@@ -6,7 +6,7 @@ function setup(){
     inputtext=createInput();
     inputtext.position(200,height-80);
     inputtext.input(updateText);
-    colorpicker=createColorPicker()
+    colorpicker=createColorPicker("#")
 
 }
 function draw(){
