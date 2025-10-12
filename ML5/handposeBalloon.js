8c45a8393582c4b6564bff97c95d120490f0b3c3
Speeder421 <davidjesselim4@gmail.com>
2025-10-12 03:28:22 +0000

    Oct 12, 2025, 3:28 AM

--- ML5/handposeBalloon.js
+++ ML5/handposeBalloon.js
@@ -29,6 +29,7 @@ function setup(){
     bottomwall=new Sprite(videoW/2,videoH,videoW,10,'static');
     leftwall=new Sprite(0,videoH/2,10,videoH,'static');
     rightwall=new Sprite(videoW,videoH/2,10,videoH,'static');
+    boundaryGroup=
 
     fingerTip=new Sprite();
     fingerTip.diameter=60;
