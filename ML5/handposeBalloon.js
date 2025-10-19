702fbcfffa8254b91afbbc44016477d4a7d0ec03
Speeder421 <davidjesselim4@gmail.com>
2025-10-19 03:27:59 +0000

    Oct 19, 2025, 3:27 AM

--- ML5/handposeBalloon.js
+++ ML5/handposeBalloon.js
@@ -66,7 +66,7 @@ function draw(){
     // }
     if (gameStarted===false){
         textSize(28);
-        text.align(CENTER,CENTER);
+        textAlign(CENTER,CENTER);
         text("Use index finger to bounce the ball",width/2,height/2-40);
         text("Press space to start game",width/2,height/2-40);
     }
