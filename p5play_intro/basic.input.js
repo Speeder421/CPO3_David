2b170897ad6a2efbb2c0b4938333dea8cb5e8e68
Speeder421 <davidjesselim4@gmail.com>
2025-11-02 03:34:59 +0000

    Nov 2, 2025, 3:34 AM

--- p5play_intro/basic.input.js
+++ p5play_intro/basic.input.js
@@ -16,7 +16,7 @@ function setup(){
 
 }
 function draw(){
-    background(225);
+    background(colorpicker.value());
     fill(colorpicker.value());
     textSize(28);
     textAlign(CENTER,CENTER);
