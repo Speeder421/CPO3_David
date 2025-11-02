24e454a250010df6512326c3450739f8d0781e70
Speeder421 <davidjesselim4@gmail.com>
2025-11-02 03:56:23 +0000

    Nov 2, 2025, 3:56 AM

---
+++ p5play_intro/basic.input.js
@@ -0,0 +1,31 @@
+let inputtext;
+
+let usertext="";
+
+let colorpicker;
+
+function setup(){
+    createCanvas(600,400);
+    inputtext=createInput();
+    inputtext.position(700,500);
+    inputtext.input(updateText);
+
+    colorpicker=createColorPicker("#63claff");
+    colorpicker.position(480,170);
+
+
+}
+function draw(){
+    background(2);
+    fill(colorpicker.value());
+    textSize(28);
+    textAlign(CENTER,CENTER);
+    text(usertext,width/2,180);
+
+
+
+
+}
+function updateText(){
+    usertext=this.value();
+}
