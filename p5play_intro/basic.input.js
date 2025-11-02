ba20ea9a9a676a5747dcc5899bcbd3bfdc680783
Speeder421 <davidjesselim4@gmail.com>
2025-11-02 04:06:06 +0000

    Nov 2, 2025, 4:06 AM

--- p5play_intro/basic.input.js
+++ p5play_intro/basic.input.js
@@ -12,18 +12,12 @@ function setup(){
     noun=createInput("Enter a noun");
     noun.position(width/2,100);
     noun.input(nountext);
-
-
-
 }
 function draw(){
     background(225);
     textSize(28);
     textAlign(CENTER,CENTER);
     text(usertext,width/2,180);
-
-
-
 
 }
 function nountext(){
