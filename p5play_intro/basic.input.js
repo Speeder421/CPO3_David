9446969b5e96fbabed78803a15c99645dc4e2d89
Speeder421 <davidjesselim4@gmail.com>
2025-11-02 03:26:33 +0000

    Nov 2, 2025, 3:26 AM

--- p5play_intro/basic.input.js
+++ p5play_intro/basic.input.js
@@ -1,43 +1,43 @@
-// let inputtext;
-// let inputtext2;
-// let usertext="";
-// let usertext2="";
-// let colorpicker;
+let inputtext;
+let inputtext2;
+let usertext="";
+let usertext2="";
+let colorpicker;
 
-// function setup(){
-//     createCanvas(600,400);
-//     inputtext=createInput();
-//     inputtext.position(700,500);
-//     inputtext.input(updateText);
+function setup(){
+    createCanvas(600,400);
+    inputtext=createInput();
+    inputtext.position(700,500);
+    inputtext.input(updateText);
 
-//     inputtext2.position(480,480);
-//     inputtext2.input(updateText2);
-//     inputtext2.input(updateText2);
+    inputtext2.position(480,480);
+    inputtext2.input(updateText2);
+    inputtext2.input(updateText2);
 
-//     colorpicker=createColorPicker("#63claff");
-//     colorpicker.position(480,170);
-
-
-// }
-// function draw(){
-//     background(colorpicker.value());
-//     fill(0);
-//     textSize(28);
-//     textAlign(CENTER,CENTER);
-//     text(usertext,width/2,180);
-
-//     fill(0);
-//     textSize(28);
-//     textAlign(CENTER,CENTER);
-//     text(usertext2,width/2,180);
+    colorpicker=createColorPicker("#63claff");
+    colorpicker.position(480,170);
 
 
+}
+function draw(){
+    background(colorpicker.value());
+    fill(0);
+    textSize(28);
+    textAlign(CENTER,CENTER);
+    text(usertext,width/2,180);
 
-// }
-// function updateText(){
-//     usertext=this.value();
-// }
+    fill(0);
+    textSize(28);
+    textAlign(CENTER,CENTER);
+    text(usertext2,width/2,180);
 
-// function updateText2(){
-//     usertext2=this.value();
-// }
+
+
+}
+function updateText(){
+    usertext=this.value();
+}
+
+function updateText2(){
+    usertext2=this.value();
+}
